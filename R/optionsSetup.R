4710a6ce41d6365e944ccda3d6b94ab6e008074c
Eliot McIntire <eliotmcintire@gmail.com>
2022-09-12 14:09:25 -0700

    tweak setLinuxBinaryRepo -- no more CRAN arg

--- R/optionsSetup.R
+++ R/optionsSetup.R
@@ -3,23 +3,17 @@
 #' Enable use of binary package builds for Linux from the Rstudio Package Manager repo.
 #' This will set the `repos` option, affecting the current R session.
 #'
-#' @param CRAN The default CRAN repository to use, when not using a binary Linux repo.
 #' @param binaryLinux A CRAN repository serving binary Linux packages.
 #'
 #' @export
-setLinuxBinaryRepo <- function(CRAN = getOption("repos", "https://cloud.r-project.org/"),
-                               binaryLinux = "https://packagemanager.rstudio.com/") {
-  options("repos" = c(CRAN = CRAN))
-
+setLinuxBinaryRepo <- function(binaryLinux = "https://packagemanager.rstudio.com/") {
   if (Sys.info()["sysname"] == "Linux" && grepl("Ubuntu", utils::osVersion)) {
-    options(
-      repos = c(
-        CRAN = if (!grepl("R Under development", R.version.string) && getRversion() >= "4.1") {
-          paste0(binaryLinux, "all/__linux__/", system("lsb_release -cs", intern = TRUE), "/latest")
-        } else {
-          CRAN
-        }
+    if (!grepl("R Under development", R.version.string) && getRversion() >= "4.1") {
+      options(
+        repos = c(
+            paste0(binaryLinux, "all/__linux__/", system("lsb_release -cs", intern = TRUE), "/latest")
+        )
       )
-    )
+    }
   }
 }
